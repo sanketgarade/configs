a54930591a953458f734aa733d6aec9c92cd664f
Sanket Garade <garade@pm.me>
2021-02-08 20:55:34 +0900

    local configs copy to repo using script
    - and minor changes in vimrc and gitconfig

---
+++ upload.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+# run below command from the shell prompt for first time to make this file an executable
+# chmod 755 xyz.sh
+
+# copy local configs to the repo folder
+cp ~/.zshrc .zshrc
+cp ~/.gitconfig .gitconfig
+cp ~/.vim/vimrc .vim/vimrc
+cp ~/.bashrc .bashrc
+cp ~/.bash_profile .bash_profile
+cp ~/.octaverc .octaverc
